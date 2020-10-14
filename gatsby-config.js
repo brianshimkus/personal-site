09eeaaefebd88d264cf89869a5ac6fc0e6c2f32a
Brian Shimkus <brianshimkus1@gmail.com>
2020-10-14 01:16:08 -0500

    Set up Gatsby image and imported hero image photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,18 +5,28 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Brian Shimkus | Front-End Developer",
+  },
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: "images",
+      },
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: "Raleway",
-            weights: ["100,200,300,400,500,600,700,800"],
-          },
+          `Raleway`,
+          `source sans pro\:100,200,300,400,500,600,700,800`, // you can also specify font weights and styles
         ],
+        display: "swap",
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,40 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container">
-          <h1>
-            <span>Brian</span> Shimkus
-          </h1>
-          <h2>Front-End Developer</h2>
+          <div class="columns">
+            <div class="column">
+              <Img
+                fluid={data.heroImageGatsbyImage.childImageSharp.fluid}
+                alt="A self portrait of Brian Shimkus"
+              />
+            </div>
+            <div class="column">
+              <h1>
+                <span>Brian</span> Shimkus
+              </h1>
+              <h2>Front-End Developer</h2>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   )
 }
+
+export const query = graphql`
+  query {
+    heroImageGatsbyImage: file(relativePath: { eq: "Me-BW.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
