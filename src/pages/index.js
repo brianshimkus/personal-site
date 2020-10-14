086fd832d65639b8edce9702470453b64c9b8ef7
Brian Shimkus <brianshimkus1@gmail.com>
2020-10-14 01:56:24 -0500

    Various style updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import {
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+  FaEnvelope,
+  FaListAlt,
+} from "react-icons/fa"
 
 export default function Home({ data }) {
   return (
@@ -19,6 +26,52 @@ export default function Home({ data }) {
                 <span>Brian</span> Shimkus
               </h1>
               <h2>Front-End Developer</h2>
+              <div className="social-icons">
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/brianshimkus"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaGithub />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/brianshimkus1/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaLinkedin />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/brianshimkus.me/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaInstagram />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="mailto:Brian@BrianShimkus.com">
+                      <FaEnvelope />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="contact">
+                <a href="../src/assets/BrianShimkus-Resume.pdf" target="_blank">
+                  <button class="button btn-primary">
+                    <span>Resume</span>
+                    <span class="icon">
+                      <FaListAlt />
+                    </span>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
