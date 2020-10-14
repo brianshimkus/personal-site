9c1c93a8c932c37241b1a005f922acc559568978
Brian Shimkus <brianshimkus1@gmail.com>
2020-10-13 23:38:52 -0500

    Initial page scaffold and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <h1>
+            <span>Brian</span> Shimkus
+          </h1>
+          <h2>Front-End Developer</h2>
+        </div>
+      </div>
+    </section>
+  )
 }
